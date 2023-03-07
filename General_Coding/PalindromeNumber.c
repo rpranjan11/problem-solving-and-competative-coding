549e44e0560569df1bedb5abfede208a76f8ab00
Ranjan Ram Pratap <rpranjan11@gmail.com>
2023-03-07 13:58:51 +0900

    added PalindromeNumber.c

---
+++ General_Coding/PalindromeNumber.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[]) {
+    int i = 0, j = strlen(str) - 1;
+    while (i < j) {
+        if (str[i] != str[j]) return 0;
+        i++; j--;
+    }
+    return 1;
+}
+
+int main() {
+    char str[100] = "radar";
+    if (isPalindrome(str))
+        printf("%s is a palindrome\n", str);
+    else
+        printf("%s is not a palindrome\n", str);
+    return 0;
+}
