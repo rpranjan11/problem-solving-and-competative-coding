9acbb73dda559aa776fb78084a103b1fe047779a
Ranjan Ram Pratap <rpranjan11@gmail.com>
2023-05-10 18:21:22 +0900

    added DfsTraversalGraph.c

---
+++ General_Coding/DfsTraversalGraph.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define V 4
+
+void DFS(int graph[V][V], int v, int visited[]) {
+    visited[v] = 1;
+    printf("%d ", v);
+    for (int i = 0; i < V; i++)
+        if (graph[v][i] && !visited[i])
+            DFS(graph, i, visited);
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 1, 0, 0},
+        {1, 0, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 0}
+    };
+    int visited[V] = {0};
+    DFS(graph, 0, visited);
+    return 0;
+}
