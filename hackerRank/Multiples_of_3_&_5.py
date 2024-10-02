da2ce7720c8c829b60b378bcadbad40f0970c720
Ranjan Ram Pratap [란잔] <rpranjan11@gmail.com>
2024-10-02 14:58:30 +0900

    hackerRank/Multiples_of_3_&_5.py - Update

--- hackerRank/Multiples_of_3_&_5.py
+++ hackerRank/Multiples_of_3_&_5.py
@@ -1,22 +1,17 @@
 # Problem Statement Link : https://www.hackerrank.com/contests/projecteuler/challenges/euler001/
 
-#!/bin/python3
-
 import sys
 
 testcase = int(input().strip())
 
 def CalSum(number, divisor):
-    quotient = int(number / divisor)
+    quotient = int(number // divisor)
     # print('quotient : ', quotient)
-    summation = divisor * ((quotient*(quotient+1))/2)
+    summation = divisor * ((quotient*(quotient+1)) // 2)
     # print(summation)
     return int(summation)
     
-for a in range(testcase):
+for test in range(testcase):
     number = int(input().strip())
     ans = CalSum(number-1, 3) + CalSum(number-1, 5) - CalSum(number-1, 15)
     print(ans)
-
-
-
