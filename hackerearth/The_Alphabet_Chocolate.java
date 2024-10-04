d6bc08ada768f49dcc9721f4a33697c85aa32a3b
Ranjan Ram Pratap [란잔] <rpranjan11@gmail.com>
2024-10-04 12:11:09 +0900

    hackerearth/The_Alphabet_Chocolate.java

---
+++ hackerearth/The_Alphabet_Chocolate.java
@@ -0,0 +1,28 @@
+// Problem Statement Link : https://www.hackerearth.com/problem/algorithm/count-vowels-1-1da7c4d0/
+
+import java.util.Scanner;
+class Solution {
+    public static void main(String args[] ){
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-- >0) {
+            String s = in.next();
+            long sum = 0;
+            long n = s.length();
+            for(int i = 0; i < n; i++) {
+                if(isVowel(s.charAt(i))) {
+                    sum += (n-i) * (i+1);
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+    static boolean isVowel(char c) {
+        switch(c) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                return true;
+        }
+        return false;
+    }
+}
